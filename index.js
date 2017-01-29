0d7dca972bfef597cbbc32351b555e57a5b3a6e7
Grigory Petrov <grigory.v.p@gmail.com>
2017-01-29 09:34:46 +0000

    meta parser stub; meta is defined like {foo} or {foo:bar}

--- index.js
+++ index.js
@@ -55,15 +55,9 @@ function logTextToCommitsAsync(text) {
     const commitList = [];
     const next = () => { let v = new Commit(); commitList.push(v); return v; }
     const cur = () => commitList.slice(-1)[0] || next();
-    const patternmatch = (string, pairs) => {
-      for ([regexp, handler] of pairs) {
-        const match = string.match(regexp);
-        if (match) return handler(...match.slice(1));
-      }
-    };
 
     for(line of text.split(/\r*\n+/)) {
-      patternmatch(line, [
+      pmatch(line, [
         [/^commit ([a-z0-9]+)$/, (hash) => (next().hash = hash)],
         [/^    (.*)$/, (line) => cur().addMsgLine(line)],
       ]);
@@ -73,10 +67,52 @@ function logTextToCommitsAsync(text) {
 }
 
 
+class Meta {
+  constructor() {
+    this.hash = '';
+    this.key = '';
+  }
+}
+
+
 function commitsToMetaAsync(commitList) {
   return new Promise((resolve, reject) => {
+
     const metaList = [];
+    const next = () => { let v = new Meta(); metaList.push(v); return v; }
+    const cur = () => commitList.slice(-1)[0] || next();
+
+    const State = {
+      IDLE: 1,
+      KEY: 2,
+    };
+    let state = State.IDLE;
+
     for (commit of commitList) {
+      for (var i = 0; i < commit.msg.length; i ++) {
+        const char = commit.msg[i];
+        pmatch(char, [
+          ['{', () => {
+            pmatch(state, [
+              [State.IDLE, () => {
+                state = State.KEY;
+                next();
+              }],
+              [null, () => { throw new Error("'{' outside of IDLE"); }],
+            ]);
+          }],
+          ['}', () => {
+            pmatch(state, [
+              [State.KEY, () => state = State.IDLE],
+            ]);
+          }],
+          [null, () => {
+            pmatch(state, [
+              [State.KEY, (v) => cur().key += v],
+            ]);
+          }],
+        ]);
+      }
     }
     resolve(metaList);
   });
@@ -94,5 +130,23 @@ function metaToJsonAsync(metaList) {
   return new Promise((resolve, reject) => {
     resolve(metaList);
   });
+}
+
+
+function pmatch(tomatch, pairs) {
+  let defaultHandler = null;
+  for ([matchwith, handler] of pairs) {
+    if (typeof(matchwith) === 'string' || typeof(matchwith) === 'number') {
+      if (matchwith === tomatch) return handler(tomatch);
+    }
+    else if (!matchwith) {
+      defaultHandler = handler;
+    }
+    else {
+      const match = tomatch.match(matchwith);
+      if (match) return handler(...match.slice(1));
+    }
+  }
+  if (defaultHandler) return defaultHandler(tomatch);
 }
 
