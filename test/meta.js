9fd806db099280198d8b17f204232ff32e5cc8e3
Grigory Petrov <grigory.v.p@gmail.com>
2017-03-01 15:32:36 +0000

    key trim test

--- test/meta.js
+++ test/meta.js
@@ -9,6 +9,7 @@ describe("commit message parser", () => {
     const test = `{foo:bar}`;
     readMetaAsync([new Commit({msg: test})]).then((commitList) => {
       expect(commitList).not.empty;
+      expect(commitList[0].metaList).to.have.length(1);
       const meta = commitList[0].metaList[0];
       expect(meta).to.have.property('key').equal('foo');
       expect(meta).to.have.property('val').equal('bar');
@@ -22,8 +23,23 @@ describe("commit message parser", () => {
     const test = `{"{\\":\\';\\\\="}`;
     readMetaAsync([new Commit({msg: test})]).then((commitList) => {
       expect(commitList).not.empty;
+      expect(commitList[0].metaList).to.have.length(1);
       const meta = commitList[0].metaList[0];
       expect(meta).to.have.property('key').equal(`{":';\\=`);
+      next();
+    }).catch((err) => {
+      next(new Error(err));
+    });
+  });
+
+  it("should trim keys", (next) => {
+    const test = `{ foo : "foo"; bar : "bar"}`;
+    readMetaAsync([new Commit({msg: test})]).then((commitList) => {
+      console.log(JSON.stringify(commitList));
+      expect(commitList).not.empty;
+      expect(commitList[0].metaList).to.have.length(2);
+      expect(commitList[0].metaList[0]).to.have.property('key').equal('foo');
+      expect(commitList[0].metaList[1]).to.have.property('key').equal('bar');
       next();
     }).catch((err) => {
       next(new Error(err));
