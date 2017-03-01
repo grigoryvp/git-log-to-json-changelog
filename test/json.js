8040ff7f990cce0ba9ef522653e07a4621133c28
Grigory Petrov <grigory.v.p@gmail.com>
2017-03-01 15:23:41 +0000

    test for new message fold logic

---
+++ test/json.js
@@ -0,0 +1,28 @@
+const expect = require('chai').expect;
+const assert = require('chai').assert;
+const metaToJsonAsync = require('./../index.js').debug.metaToJsonAsync;
+const Commit = require('./../index.js').debug.Commit;
+const Meta = require('./../index.js').debug.Meta;
+
+
+describe("json generator", () => {
+
+  it("should user last of multiple messages per commit", (next) => {
+    const commitList = [
+      new Commit({hash: '123', metaList: [
+        new Meta({hash: '123', key: 'msg', val: 'foo', seq: 1}),
+        new Meta({hash: '123', key: 'msg', val: 'bar', seq: 2}),
+      ]}),
+    ];
+    metaToJsonAsync(commitList).then((releaseList) => {
+      expect(releaseList).not.empty;
+      const release = releaseList[0];
+      expect(release).to.have.property('msgList').with.length(1);
+      expect(release.msgList[0]).to.equal('bar');
+      next();
+    }).catch((err) => {
+      next(new Error(err));
+    });
+  });
+});
+
